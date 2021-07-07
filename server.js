bafe1e2f4b9a119b6ee4a903901c3d86fa046273
smg061 <max.go.95@gmail.com>
2021-07-07 18:59:20 -0500

    weird error with the port on heroku; changed the PORT declaration syntax to see if it fixes it

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const routes = require("./controllers");
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001
 const connectToDb = require("./config/mongoose");
 
 
